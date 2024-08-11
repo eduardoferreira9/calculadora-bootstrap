59cb37ff1200b909e49d9707b9d1fd3deab04c4b
Eduardo <89710660+eduardoferreira9@users.noreply.github.com>
2024-08-11 19:11:31 -0300

    Adicionado um ponteiro paar melhor indicação se está saudável ou não.

--- BMI - Calculator/script.js
+++ BMI - Calculator/script.js
@@ -2,20 +2,42 @@ function calculadora() {
     let peso = parseFloat(document.getElementById('peso').value);
     let altura = parseFloat(document.getElementById('altura').value);
     let resultado = document.getElementById('resultado_final');
+    let triangulo = document.getElementById('triangulo');
 
     if (isNaN(peso) || isNaN(altura) || altura <= 0 || peso <= 0) {
         resultado.textContent = 'Por favor, insira valores válidos para peso e altura.';
+        triangulo.style.left = '0%'; // Posição padrão em caso de erro
+        triangulo.style.borderTopColor = '#FF6D6D'; // Cor padrão para erro
         return;
     }
     
     let alturaEmMetros = altura / 100;
     let imc = peso / (alturaEmMetros ** 2);
-    
 
     resultado.textContent = `O seu IMC é de ${imc.toFixed(2)}`;
+    
+    let posicao = '0%'; // Posição padrão
+    let cor = '#FF6D6D'; // Cor padrão para erro
+
+    if (imc < 18.5) {
+        posicao = '0%';
+        cor = '#6C63FF'; // Cor correspondente
+    } else if (imc >= 18.5 && imc < 24.9) {
+        posicao = '20%'; // Ajuste conforme a largura das cores
+        cor = '#60984B';
+    } else if (imc >= 25 && imc < 29.9) {
+        posicao = '40%';
+        cor = '#8DDA71';
+    } else if (imc >= 30) {
+        posicao = '60%';
+        cor = '#FF6D6D';
+    }
+
+    triangulo.style.left = posicao;
+    triangulo.style.borderTopColor = cor;
 }
 
-
+// Inicializar a posição do triângulo
 document.addEventListener('DOMContentLoaded', function() {
     const sr = ScrollReveal({
         duration: 1000, 
@@ -26,13 +48,12 @@ document.addEventListener('DOMContentLoaded', function() {
     
     sr.reveal('.coluna-esquerda', {
         origin: 'bottom',
-    })
+    });
 
     sr.reveal('.imagem', {
         origin: 'top',
-    })
+    });
 });
-
 
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('.custom-button');
